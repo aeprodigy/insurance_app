3b1da2b5130f6a400dfe947b73e6ecd384af7a2d
Mike Mambwe <mikeapple056@gmail.com>
2025-07-26 11:43:13 +0200

    summary details page

--- src/Components/steps/SummaryStep.tsx
+++ src/Components/steps/SummaryStep.tsx
@@ -1,28 +1,65 @@
-import { useFormStore } from "../../store/formStore"
+import { useFormStore } from "../../store/formStore";
 
 const SummaryStep = () => {
-
-  //the zustand form
-  const {data} = useFormStore();
+  const { data } = useFormStore();
+//
   return (
-    <div className="space-y-2">
-      <h2 className="text-lg font-bold">Summary</h2>
-      <div className="text-sm">
-        <p>Name:{data.fullName}</p>
-        <p>Email:{data.email}</p>
-        <p>Phone:{data.phone}</p>
-        <p>
-          Vehicle:{data.vehicleMake} {data.vehicleModel}
-          {data.year}
-        </p>
-        <p>Registration Number:{data.registrationNumber}</p>
-        <p>Location:{data.location}</p>
-        <p>Cover Type:{data.coverType}</p>
-        <p>Start Date:{data.startDate}</p>
-        <p>Duration:{data.duration}</p>
+    <div className=" mx-auto bg-white shadow-md rounded-lg p-6 border border-gray-200">
+     
+      <div className="mb-1">
+        <h3 className="text-lg font-semibold text-gray-600 mb-2">
+          Client Details
+        </h3>
+        <div className="text-sm text-gray-700 space-y-1">
+          <p>
+            <span className="font-medium">Full Name:</span> {data.fullName}
+          </p>
+          <p>
+            <span className="font-medium">Email:</span> {data.email}
+          </p>
+          <p>
+            <span className="font-medium">Phone:</span> {data.phone}
+          </p>
+        </div>
+      </div>
+
+      <div className="mb-1">
+        <h3 className="text-lg font-semibold text-gray-600 mb-2">
+          Vehicle Details
+        </h3>
+        <div className="text-sm text-gray-700 space-y-1">
+          <p>
+            <span className="font-medium">Make & Model:</span>{" "}
+            {data.vehicleMake} {data.vehicleModel} {data.year}
+          </p>
+          <p>
+            <span className="font-medium">Registration No.:</span>{" "}
+            {data.registrationNumber}
+          </p>
+          <p>
+            <span className="font-medium">Location:</span> {data.location}
+          </p>
+        </div>
+      </div>
+
+      <div className="mb-1">
+        <h3 className="text-lg font-semibold text-gray-600 mb-2">
+          Insurance Details
+        </h3>
+        <div className="text-sm text-gray-700 space-y-1">
+          <p>
+            <span className="font-medium">Cover Type:</span> {data.coverType}
+          </p>
+          <p>
+            <span className="font-medium">Start Date:</span> {data.startDate}
+          </p>
+          <p>
+            <span className="font-medium">Duration:</span> {data.duration}
+          </p>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default SummaryStep
+export default SummaryStep;
